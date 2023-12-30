67fc4d06260e104aedf7e88d5279a2fd8b4ef7e5
包子 <chengbaozi66@qq.com>
2023-12-30 20:13:00 +0800

    GUI_Button

---
+++ src/APP_UI.h
@@ -0,0 +1,31 @@
+#ifndef APP_UI_H__
+#define APP_UI_H__
+
+#include "PN_Login.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct CtxUI
+{
+    PN_Login *pn_login;
+    /* data */
+}CtxUI;
+
+void APP_UI_Free(CtxUI *ctx){
+    if(ctx->pn_login!=NULL){
+        free(ctx->pn_login);
+        ctx->pn_login=NULL;
+    }
+    free(ctx);
+}
+
+//画
+void APP_UI_Draw(CtxUI *ctx){
+    if(ctx->pn_login!=NULL){
+        PN_Login_Draw(ctx->pn_login);
+    }
+}
+
+
+#endif
---
+++ src/PN_Login.h
@@ -0,0 +1,27 @@
+#ifndef PN_LOGIN_H__
+#define PN_LOGIN_H__
+
+
+#include "../include/GUI_Button.h"
+#include "Common.h"
+
+typedef struct PN_Login {
+    GUI_Button btn_Start;
+} PN_Login;
+
+
+//生成
+void PN_Login_Ctor(PN_Login *panel){
+    panel->btn_Start=GUI_Button_New(Vector2_New(480,270),Vector2_New(160,30),BLACK,GRAY,WHITE,"Login");
+}
+
+void PN_Login_Draw(PN_Login *panel ){
+    GUI_Button_Draw(&panel->btn_Start);
+}
+
+bool PN_Login_IsStartClick(PN_Login *panel ){
+    return GUI_Button_IsClick(&panel->btn_Start);
+}
+
+
+#endif
